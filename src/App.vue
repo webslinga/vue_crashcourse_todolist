b75b437e3c3c6853b3814bef26c1db9fb1d941a7
Zaman Harris <zharris0503@gmail.com>
2020-12-08 23:36:35 -0500

    Add items with complete and delete feature.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
 <template>
   <div id="app">
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos';
 
 export default {
+  name: 'app',
   components: {
+    Todos
+  },
+  data(){
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "To Do One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "To Do Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "To Do Three",
+          completed: false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
   }
 }
 </script>
 
 <style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
